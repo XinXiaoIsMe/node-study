98229d44b9dfb9f2c6e4939cd255b6932b993563
XinXiaoIsMe <ccattempt@163.com>
2025-08-18 23:51:44 +0800

    update: zlib

--- zlib/index.js
+++ zlib/index.js
@@ -3,8 +3,11 @@ const {
   createGzip,
   createGunzip,
   createDeflate,
-  createInflate
+  createInflate,
+  gzipSync,
+  deflateSync
 } = require('node:zlib');
+const { createServer } = require('node:http');
 
 // gzip压缩
 // createReadStream('./test.txt')
@@ -22,6 +25,27 @@ const {
 //   .pipe(createWriteStream('./test.txt.deflate'))
 
 // Deflate解压缩
-createReadStream('./test.txt.deflate')
-  .pipe(createInflate())
-  .pipe(createWriteStream('./test3.txt'))
+// createReadStream('./test.txt.deflate')
+//   .pipe(createInflate())
+//   .pipe(createWriteStream('./test3.txt'))
+
+const server = createServer((req, res) => {
+  const txt = '测试文字'.repeat(1000);
+  res.setHeader('Content-type', 'text/plain;charset=utf-8');
+  // res.end(txt); // 12.2kb
+
+  // 使用gzip压缩
+  // res.setHeader('Content-Encoding', 'gzip')
+  // res.end(gzipSync(txt)); // 0.3kb
+
+  // 使用deflate压缩
+  res.setHeader('Content-Encoding', 'deflate')
+  res.end(deflateSync(txt)); // 0.3kb
+
+  // deflate压缩会比gzip压缩更快一点，更小一点，但是差距不大
+  // gzip适合压缩文件（无损），deflate适合压缩http里面编码的内容
+});
+
+server.listen(3000, () => {
+  console.log('服务已经启动：http://localhost:3000')
+}); // 端口号小于65535都可以用
