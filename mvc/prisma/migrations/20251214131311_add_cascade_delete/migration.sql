48a34d4ad3ee63a12c4a42df1e350622c9696d50
xin.xiao <xin.xiao@udschina.com>
2025-12-15 11:39:56 +0800

    feat(mvc): split database table

--- mvc/prisma/migrations/20251214131311_add_cascade_delete/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `post` DROP FOREIGN KEY `Post_authorId_fkey`;
-
--- DropIndex
-DROP INDEX `Post_authorId_fkey` ON `post`;
-
--- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
