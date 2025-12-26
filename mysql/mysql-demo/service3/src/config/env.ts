7b58ef0c73f17cf2f85b83ddb4b6910cff8de0cd
xin.xiao <xin.xiao@udschina.com>
2025-12-26 15:57:20 +0800

    refactor(service3): set default port

--- mysql/mysql-demo/service3/src/config/env.ts
+++ mysql/mysql-demo/service3/src/config/env.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
     DATABASE_USER: z.string(),
     DATABASE_PASSWORD: z.string(),
     DATABASE_NAME: z.string(),
-    PORT: z.string().transform(port => Number(port)),
+    PORT: z.coerce.number().default(3000),
     JWT_SECRET: z.string(),
     JWT_ACCESS_TOKEN_EXPIRES_IN: z.string(),
 });
